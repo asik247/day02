adaaed77af727ee989f462761196d69ed59e149d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-01-04 00:12:28 +0600

    revision

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 // AritchMetich Operator example
-// let a = 5;
-// let b = (100+50)*a;
-// let result = String(b)
-// console.log(result);
-// console.log(typeof(result));
-// let x = 5;
-// let y = x+"";
-// console.log(y);
+let a = 5;
+let b = (100+50)*a;
+let result = String(b)
+console.log(result);
+console.log(typeof(result));
+let x = 5;
+let y = x+"";
+console.log(y);
 
-// let k=null;
-// console.log(typeof(k));
+let k=null;
+console.log(typeof(k));
 
 
-// let w = null;
-// console.log(String(w));
+let w = null;
+console.log(String(w));
 // type chake a toString null and undifind a erro show kore but string a erro dai na null hole null undifind hole undifind.
 
 // js string addition
-// let tex1 = 'john';
-// let tex2 = 'doe';
+let tex1 = 'john';
+let tex2 = 'doe';
 
-// let tex3 = tex1 + " " + tex2;
-// let tex4 = tex1 + tex2;
-// console.log(tex3);
-// console.log(tex4);
+let tex3 = tex1 + " " + tex2;
+let tex4 = tex1 + tex2;
+console.log(tex3);
+console.log(tex4);
 
 // number and string auto convert
-// let age = 23;
-// let text = "Age is: " + age;
-// console.log(text);
-// console.log(typeof(text));
+let age = 23;
+let text = "Age is: " + age;
+console.log(text);
+console.log(typeof(text));
 
 
 // multiple value concaenate
@@ -55,3 +55,7 @@ console.log(text);
 let price = 750;
 let message = "Totla price: " + String(price) + " taka";
 console.log(message);
+
+let num = 20;
+let str = "5";
+console.log(num+str);
