686aad4cb21ca3d191e68836d5771244eb34997d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-01-03 23:52:42 +0600

    concate

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 // AritchMetich Operator example
-let a = 5;
-let b = (100+50)*a;
-let result = String(b)
-console.log(result);
-console.log(typeof(result));
-let x = 5;
-let y = x+"";
-console.log(y);
+// let a = 5;
+// let b = (100+50)*a;
+// let result = String(b)
+// console.log(result);
+// console.log(typeof(result));
+// let x = 5;
+// let y = x+"";
+// console.log(y);
 
-let k=null;
-console.log(typeof(k));
+// let k=null;
+// console.log(typeof(k));
 
 
-let w = null;
-console.log(String(w));
+// let w = null;
+// console.log(String(w));
 // type chake a toString null and undifind a erro show kore but string a erro dai na null hole null undifind hole undifind.
+
+// js string addition
+// let tex1 = 'john';
+// let tex2 = 'doe';
+
+// let tex3 = tex1 + " " + tex2;
+// let tex4 = tex1 + tex2;
+// console.log(tex3);
+// console.log(tex4);
+
+// number and string auto convert
+let age = 23;
+let text = "Age is: " + age;
+console.log(text);
+console.log(typeof(text));
+
+
+// multiple value concaenate
+let a = 20
+let b = 20
+let result = "total = " + a+b;
+// right way
+let r = "total is = " +(a+b)
+console.log(result);
+console.log(r);
+
+// 
