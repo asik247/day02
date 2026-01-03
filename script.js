a79459d3c6412d8069f83d9ca9fd8705e1bd6ca9
Asikur Rahaman <mdasikur5893@gmail.com>
2026-01-03 23:40:40 +0600

    operator

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // AritchMetich Operator example
 let a = 5;
 let b = (100+50)*a;
-console.log(b);
+let result = String(b)
+console.log(result);
+console.log(typeof(result));
+let x = 5;
+let y = x+"";
+console.log(y);
+
+let k=null;
+console.log(typeof(k));
+
+
+let w = null;
+console.log(String(w));
+// type chake a toString null and undifind a erro show kore but string a erro dai na null hole null undifind hole undifind.
